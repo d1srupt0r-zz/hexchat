e91f3c9aadc911325cf5e624ca8d23d3234fc891
d1srupt0r <disruptorq2k@yahoo.com>
2016-01-21 00:56:18 -0800

    finally made progress; may need cleanup

--- colorize.pl
+++ colorize.pl
@@ -25,35 +25,40 @@ for my $event (@events) {
 	hook_print($event, \&colorize, { data => $event, priority => PRI_HIGH});
 }
 
+my @rcolors = ( 19, 20, 22, 24, 25, 26, 27, 28, 29 );
 my $exit;
+
+hook_command("colorz", \&colorz, { help_text => "Usage: /colorz to show a list of available colors."});
+
+sub colorz {
+	for my $color (@rcolors) {
+		HexChat::printf("\cC%d%s", $color, $color);
+	}
+	return EAT_ALL;
+}
 
 sub colorize {
 	$exit = 0;
 
 	my @msg = @{$_[0]};
 	my $event = $_[1];
-	my $nick = $msg[0];
+	my $nick = HexChat::strip_code($msg[0]);
+	my $num = &text_color_of($nick);
 
-	my $num = &text_color_of(HexChat::strip_code($nick));
+	#HexChat::printf("\cC%d%d | %s", $num, $num, $event);
+	my $custom = $msg[2] 
+		? sprintf("\cC%d%s\cC%d%s", 0, $msg[2], $num, $nick)
+		: sprintf("\cC%d%s", $num, $nick);
 
-	if ($nick =~ /^\003$num/) {
-		$exit = -1;
-		return EAT_NONE;
-	}
-
-	#HexChat::printf("\cC%d%d", $num, $num);
-	$msg[0] = sprintf("\cC%d%s", $num, $msg[0]);
-	HexChat::emit_print($event, @msg);# unless $exit;
-
+	HexChat::emit_print($event, $custom, $msg[1]) unless $exit;
 	return EAT_ALL;
 }
 
 sub text_color_of {
-	my @rcolors = ( 19, 20, 22, 24, 25, 26, 27, 28, 29 );
 	my $sum = 0;
 	for my $byte (split //, $_[0]) {
 		$sum += ord($byte);
 	}
 	$sum %= $#rcolors / 2;
-	return $rcolors[$sum];
+	return $rcolors[rand($sum)];
 }
