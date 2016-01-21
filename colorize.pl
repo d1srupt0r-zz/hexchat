ccebad5fd8661f78f2ca98b1b5395f50775257fc
d1srupt0r <disruptorq2k@yahoo.com>
2016-01-20 21:55:15 -0800

    remove some debug code; still needs work

--- colorize.pl
+++ colorize.pl
@@ -35,9 +35,15 @@ sub colorize {
 	my $nick = $msg[0];
 
 	my $num = &text_color_of(HexChat::strip_code($nick));
-	HexChat::printf("\cC%d%d", $num, $num);	
+
+	if ($nick =~ /^\003$num/) {
+		$exit = -1;
+		return EAT_NONE;
+	}
+
+	#HexChat::printf("\cC%d%d", $num, $num);
 	$msg[0] = sprintf("\cC%d%s", $num, $msg[0]);
-	emit_print($event, @msg) unless $exit;
+	HexChat::emit_print($event, @msg);# unless $exit;
 
 	return EAT_ALL;
 }
@@ -47,8 +53,7 @@ sub text_color_of {
 	my $sum = 0;
 	for my $byte (split //, $_[0]) {
 		$sum += ord($byte);
-		#HexChat::printf("%s", ord($byte));
 	}
 	$sum %= $#rcolors / 2;
-	return $rcolors[$sum];#sprintf("\cC%s%s", 24, HexChat::strip_code($_[0]))
+	return $rcolors[$sum];
 }
